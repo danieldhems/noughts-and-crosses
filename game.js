3cd97d82c1a493375cdf8977bfa31de894c22aaa
Dan Hems <danhems87@gmail.com>
2014-12-22 07:58:13 +0000

    WIP: Taking simple route for realtime play.

--- game.js
+++ game.js
@@ -11,49 +11,13 @@ game ends when a line is completed
 
 */
 
-window.onload = function(){
+function NoughtsAndCrosses(){
 
-	var socket = io(),
-			host = null,
-			hostSymbol = null,
-			guestSymbol = null;
+	var self = this;
 
-	var username,
-			roomname;
+	self.socket = io();
 
-	var gameInfo = document.querySelector("#gameInfo");
-
-	var form = document.forms["gameOptions"];
-
-	form.addEventListener("submit", function(e){
-		e.preventDefault();
-		
-		var username = form.username.value,
-				roomname = form.roomname.value;
-
-		socket.emit("initGame", {
-			username: username,
-			roomname: roomname
-		});
-	});
-
-	socket.on("hostJoin", function(data){
-		var joinText = document.createElement("p").innerHTML;
-
-		hostSymbol = "0";
-		joinText = "You created "+data.roomname;
-
-		console.log(data);
-		gameInfo.appendChild(joinText);
-	});
-
-	socket.on("userJoin", function(data){
-		var joinText = document.createElement("p"),innerHTML;
-		joinText = data.username + " joined";
-		guestSymbol = "X";
-	});
-
-	var winLines = [
+	self.winLines = [
 		// rows
 		[11,21,31],
 		[12,22,32],
@@ -67,10 +31,15 @@ window.onload = function(){
 		[13,22,31]
 	];
 
-	// create container for grid
-	var gameGrid = document.querySelector("#gameGrid");
+	self.gameWon = false;
+	self.winningLine = undefined;
 
-	function drawGrid(x,y){
+	self.playerSymbol = "0";
+
+	// create container for grid
+	self.gameGrid = document.querySelector("#gameGrid");
+
+	self.drawGrid = function(x,y){
 		for(var r=y; r>0; r--){
 			for(var c=0; c<x; c++){
 				var cell = document.createElement('span');
@@ -81,48 +50,49 @@ window.onload = function(){
 	}
 
 	// handle each play
-	function clickHandler(e){
-		var elem = e.target,
-			thisCell = getGridRef(elem);
+	self.clickHandler = function(e){
+		var elem = e.target;
+		
+		var thisCell = self.getGridRef(elem);
 
-		if(elem.innerHTML == "") elem.innerHTML = playerSymbol;
+		if(elem.innerHTML == "") elem.innerHTML = self.playerSymbol;
 
-		checkWinningLine(elem);
+		self.checkWinningLine(elem);
 
 		var moveData = {
 			cell: thisCell
 		};
 
-		socket.emit("move", moveData);
+		self.socket.emit("move", moveData);
 	}
 
 	// get grid reference from ID of last played cell
 	// @return two-digit Number
-	function getGridRef(elem){
+	self.getGridRef = function(elem){
 		return parseInt(elem.id.substring(1), 10);
 	}
 
 	// attach click handler to all cells in game grid
-	function bindClicks(gameGrid){
+	self.bindClicks = function(gameGrid){
 		var cells = gameGrid.querySelectorAll('span');
 
 		for(var i=0; i<cells.length; i++){
-			cells[i].addEventListener('click', clickHandler);
+			cells[i].addEventListener('click', self.clickHandler);
 		}
 	}
 
 	// find lines that intersect the last click
 	// we only need to check these lines for a winning move
-	function getIntersectingLines(currentCell){
+	self.getIntersectingLines = function(currentCell){
 
 		// remove first letter from id and convert grid reference to array
-		var target = getGridRef(currentCell),
+		var target = self.getGridRef(currentCell),
 			lines = [];
 
 		// check all winlines for target cell and store in lines array	
-		for(var line in winLines){
-			if(winLines[line].indexOf(target)>-1){
-				lines.push(winLines[line]);
+		for(var line in self.winLines){
+			if(self.winLines[line].indexOf(target)>-1){
+				lines.push(self.winLines[line]);
 			}
 		}
 
@@ -130,58 +100,66 @@ window.onload = function(){
 	}
 
 	// look for winning lines for the last played cell
-	function checkWinningLine(target){
+	self.checkWinningLine = function(target){
 		// check only intersecting lines for current player
 
-		var lines = getIntersectingLines(target);
+		var lines = self.getIntersectingLines(target);
 
 		for(var line in lines){
 			var currentLine = lines[line];
 
-			var cell1 = gameGrid.querySelector("#c"+currentLine[0]),
-				cell2 = gameGrid.querySelector("#c"+currentLine[1]),
-				cell3 = gameGrid.querySelector("#c"+currentLine[2]);
-
+			var cell1 = self.gameGrid.querySelector("#c"+currentLine[0]),
+				cell2 = self.gameGrid.querySelector("#c"+currentLine[1]),
+				cell3 = self.gameGrid.querySelector("#c"+currentLine[2]);
 
 			if(
-				cell1.innerHTML == playerSymbol &&
-				cell2.innerHTML == playerSymbol &&
-				cell3.innerHTML == playerSymbol
+				cell1.innerHTML == self.playerSymbol &&
+				cell2.innerHTML == self.playerSymbol &&
+				cell3.innerHTML == self.playerSymbol
 			){
-				removeEvents();
-				finishGame(currentLine);
+				self.removeEvents();
+				self.finishGame(currentLine);
 			}
 		}
 	}
 
-	function removeEvents(){
-		var cells = gameGrid.querySelectorAll('span');
+	self.removeEvents = function(){
+		var cells = self.gameGrid.querySelectorAll('span');
 		for(var i=0; i<cells.length; i++){
-			cells[i].removeEventListener('click', clickHandler);
+			cells[i].removeEventListener('click', self.clickHandler);
 		}
 	}
 
-	function finishGame(winningLine){
+	self.finishGame = function(winningLine){
 		for(var cell in winningLine){
-			document.querySelector("#c"+winningLine[cell]).style.backgroundColor = "green";
+			document.querySelector("#c"+winningLine[cell]).style.backgroundColor = "lime";
 		}
 	}
 
-	function updateGameGrid(move){
-		console.log(opponentSymbol);
-		var cell = gameGrid.querySelector("#c"+move.cell)
+	// Do move and check for win
+	self.doMove = function(move){
+		var cell = self.gameGrid.querySelector("#c"+move.cell)
 		// update grid with last play from opponent
 		cell.innerHTML = opponentSymbol;
 		// look for winning line 
-		checkWinningLine(cell);
+		self.checkWinningLine(cell);
 	}
 
 	// receive opponent's move and update game
-	socket.on("move", function(data){
+	self.socket.on("move", function(data){
 		console.log(data);
-		updateGameGrid(data);
+		self.doMove(data);
 	});
 
-	drawGrid(3,3);
-	bindClicks(gameGrid);
+	self.drawGrid(3,3);
+	self.bindClicks(gameGrid);
+
+	return self;
+
+};
+
+window.onload = function(){
+	var game = new NoughtsAndCrosses();
+	console.log(game);
+	game.drawGrid();
 }
--- server.js
+++ server.js
@@ -13,52 +13,15 @@ var clients = [];
 io.on("connection", function(socket){
 	var socketID = socket.id;
 
-	var roomID = "game",
-			rooms = io.sockets.adapter.rooms;
-
 	var gameInfo = {};
 
 	socket.on("initGame", function(data){
 
-		console.log(data);
-
-		// if no room name is provided, join a new room named after the connected uesr
-		if(!data.roomname){
-			roomID = data.username;
-
-			gameInfo.host = data.username;
-			gameInfo.roomname = data.username;
-			gameInfo.players = [data.username];
-			gameInfo.turn = "host";
-
-			socket.join(roomID);
-
-			io.to(data.username).emit("userJoin", {
-				username: data.username,
-				roomname: data.username,
-				host: data.username,
-				turn: gameInfo.turn
-			});
-		} 
-		// if a room name is provided, join it if it exists
-		else {
-			roomID = data.roomname;
-			socket.join(roomID);
-			io.to(data.roomname).emit("guestJoin", {
-				username: data.username,
-				roomname: gameInfo.roomname,
-				host: gameInfo.host,
-				turn: gameInfo.turn
-			});
-		}
-		console.log(rooms);
-		
 	});
 
 	socket.on("move", function(data){
-		//console.log("move received from: "+socket.id);
-		gameInfo.turn = data.turn == "0" ? "X" : "0";
-		io.to(roomID).emit("move", data);
+		console.log(data);
+		socket.broadcast.emit("move", data);
 	});
 });
 
